b3fafcac2d2175c1360bbad68689098d0debec55
Michael E. Claudino <michaeledavi12@gmail.com>
2025-05-30 22:38:23 -0300

    feat: Database and users

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,17 @@
+import { IsString, IsInt, IsEmail, IsNotEmpty, MinLength } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsString()
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(8)
+  password: string;
+}
