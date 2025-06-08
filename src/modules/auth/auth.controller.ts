ffad1af3d95023a7ce131a0186c6e9f3ef6bf766
Michael <michaeledavi12@gmail.com>
2025-06-08 11:59:32 -0300

    feat: IsPublic and ActiveUserId decorators

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -2,7 +2,9 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SigninDto } from './dto/signin.dto';
 import { SignupDto } from './dto/signup.dto'
+import { IsPublic } from 'src/shared/decorators/IsPlubic';
 
+@IsPublic()
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
@@ -13,10 +15,9 @@ export class AuthController {
   }
 
   @Post('signup')
-  create(@Body() signupDto: SignupDto) {
+  signup(@Body() signupDto: SignupDto) {
     return this.authService.signup(signupDto);
   }
-
 }
 
 
